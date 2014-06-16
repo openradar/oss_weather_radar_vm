a8b1036c6e2f6e9ef06d8db91852eef62ba74ab2
Daniel Michelson <dbm@baltrad.eu>
2014-06-16 15:46:07 +0200

    Rejiggered rave_gmaps and added a case

--- provision_scripts/install_baltrad_common.sh
+++ provision_scripts/install_baltrad_common.sh
@@ -3,12 +3,9 @@ set -x
 
 # Global Baltrad setup
 sudo apt-get install -qq git
-sudo apt-get install -qq apache2
 sudo mkdir /opt/baltrad
 sudo chown vagrant:vagrant /opt/baltrad
-sudo cp /vagrant/vendor/etc/apache2/apache2.conf /etc/apache2/apache2.conf
-sudo cp /vagrant/vendor/etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-sudo service apache2 restart
+
 cd ~
 mkdir tmp
 cd tmp
--- provision_scripts/install_baltrad_rave_gmap.sh
+++ provision_scripts/install_baltrad_rave_gmap.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -x
 
-# Vagrant provision script for installing wradlib
+# Vagrant provision script for installing BALTRAD GoogleMapsPlugin component
 export PATH="/home/vagrant/miniconda/bin:$PATH"
 
 # dependencies
 conda install --yes PIL
+sudo apt-get install -qq apache2
+sudo apt-get install -qq php5
+sudo apt-get install -qq libapache2-mod-php5
+sudo cp /vagrant/vendor/etc/apache2/apache2.conf /etc/apache2/apache2.conf
+sudo cp /vagrant/vendor/etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+sudo service apache2 restart
 
 # install GoogleMapsPlugin from source
 cd ~
@@ -18,3 +24,11 @@ python setup.py install --prefix=/opt/baltrad
 # import distutils.sysconfig
 # The .pth file is not copied because of this, manually create the file
 echo /opt/baltrad/rave_gmap/Lib/ > ~/miniconda/lib/python2.7/site-packages/rave_gmap.pth
+
+# Add an amazing case!
+cp /vagrant/vendor/opt/baltrad/rave_gmap/web/smhi-areas.xml /opt/baltrad/rave_gmap/web/.
+cp /vagrant/vendor/opt/baltrad/rave_gmap/web/products.js /opt/baltrad/rave_gmap/web/.
+mkdir /opt/baltrad/rave_gmap/web/data
+cp /vagrant/vendor/opt/baltrad/rave_gmap/web/data/eesur_gmaps.tgz /opt/baltrad/rave_gmap/web/data/.
+cd /opt/baltrad/rave_gmap/web/data
+tar xzf eesur_gmaps.tgz
