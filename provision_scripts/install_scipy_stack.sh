8ede42c0480929f0f046ad2c5fc7a53a402af4f0
Daniel Michelson <dbm@baltrad.eu>
2014-07-10 15:57:17 +0200

    Added quality plugins for beamb and bropo when installing

--- provision_scripts/install_scipy_stack.sh
+++ provision_scripts/install_scipy_stack.sh
@@ -2,6 +2,7 @@
 set -x
 
 # Vagrant provising script for installing a SciPy stack
+sudo apt-get update -qq
 sudo apt-get install -qq python-pip python-dev
 sudo apt-get install -qq python-numpy python-scipy python-matplotlib cython
 sudo apt-get install -qq python-zmq python-jinja2 python-tornado
