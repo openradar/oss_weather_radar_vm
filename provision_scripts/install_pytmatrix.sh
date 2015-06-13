f01e5b4ea7cd72ac0dce1feb46e58b2c13341314
Daniel Michelson <dbm@baltrad.eu>
2015-06-13 11:23:55 +0300

    Added PyTMatrix and its notebooks

---
+++ provision_scripts/install_pytmatrix.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+# PyTMatrix install
+
+sudo pip install pytmatrix
---
+++ provision_scripts/install_pytmatrix_short_course.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -x
+
+# Davide Ori's PyTMatrix notebooks
+
+cd ~
+git clone --depth=1 https://github.com/DaveOri/tmatrix_course.git
