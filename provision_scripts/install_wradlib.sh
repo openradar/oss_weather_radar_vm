adda610a8b866d1fcaf9749f015b8e8b25c206f6
heistermann <maik.heistermann@uni-potsdam.de>
2016-09-08 11:12:33 +0200

    MIN: Get wradlib via git clone instead of wget.
    Plus a minor modification of the wradlib course directory tree,

--- provision_scripts/install_wradlib.sh
+++ provision_scripts/install_wradlib.sh
@@ -8,25 +8,20 @@ sudo pip install numpy
 sudo apt-get install -qq libfontconfig1 python-gdal python-h5py
 sudo pip install xmltodict
 
-# Install wradlib from source
+
+# Install wradlib
 cd ~
 mkdir tmp
 cd tmp
-sudo apt-get install -qq unzip
-# Install wradlib
-wget --no-check-certificate https://github.com/wradlib/wradlib/archive/master.zip
-unzip master.zip
-cd wradlib-master
+git clone --depth=1 https://github.com/wradlib/wradlib.git
+cd wradlib
 sudo python setup.py install
-# copy course notebooks from wradlib distribution
-cp -r notebooks/ ~/wradlib_course/ 
-cd ~
-sudo rm -r tmp
-# Install wradlib data
-wget --no-check-certificate https://github.com/wradlib/wradlib-data/archive/master.zip
-unzip master.zip
-rm master.zip
-# Set environemtn variables
-echo "export WRADLIB_DATA=~/wradlib-data-master" >> ~/.profile
 echo "export GDAL_DATA=/usr/share/gdal/1.10/" >> ~/.profile
- 
+cd ~
+mkdir wradlib_short_course
+cp -a tmp/wradlib/notebooks/ ~/wradlib_short_course/
+
+# Install wradlib data
+cd wradlib_short_course
+git clone --depth=1 https://github.com/wradlib/wradlib-data.git
+echo "export WRADLIB_DATA=~/wradlib_short_course/wradlib-data" >> ~/.profile
