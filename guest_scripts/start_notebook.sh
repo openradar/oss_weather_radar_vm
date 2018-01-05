14560285d3e4508ca32ffc0699d817ed7829a20f
Maik Heistermann <maik.heistermann@uni-potsdam.de>
2018-01-05 09:23:38 +0000

    start_notebook can be called with parameter for conda env

--- guest_scripts/start_notebook.sh
+++ guest_scripts/start_notebook.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 cd ~
 
-if [$# = 1]
-    then jupyter notebook --no-browser --ip=10.0.2.15
-    else source $CONDA_DIR/bin/activate $1 && \
+if [ $# -eq 1 ]
+    then echo "Using conda environment $1" && \
+            source $CONDA_DIR/bin/activate $1 && \
             jupyter notebook --no-browser --ip=10.0.2.15
+    else echo "Using system Python" && \
+            jupyter notebook --no-browser --ip=10.0.2.15
+
 fi
