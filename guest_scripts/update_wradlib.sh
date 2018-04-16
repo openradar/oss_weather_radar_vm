2a1e03792b098d4dc6bcb19e3bbf6573d53ab81b
Maik Heistermann <maik.heistermann@uni-potsdam.de>
2018-04-16 13:14:23 +0000

    Adjust wradlib install to v1_0 and add jupyter config file

--- guest_scripts/update_wradlib.sh
+++ guest_scripts/update_wradlib.sh
@@ -6,11 +6,13 @@ set -x
 cd ~
 mkdir tmp
 cd tmp
-git clone --depth=1 https://github.com/wradlib/wradlib.git
+git clone https://github.com/wradlib/wradlib.git
 cd wradlib
 sudo python setup.py install
+
+# Install wradlib course notebooks
 cd ~
-sudo rm -rf wradlib_short_course
-mkdir wradlib_short_course
-cp -a tmp/wradlib/notebooks/ ~/wradlib_short_course/
+sudo rm -rf wradlib-notebooks
+git clone --depth=1 https://github.com/wradlib/wradlib-notebooks.git
+
 sudo rm -rf tmp
