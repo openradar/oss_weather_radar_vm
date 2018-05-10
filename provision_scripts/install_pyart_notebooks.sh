ef70bde02e117442855a5b7c99e98bca35bb220d
Zachary Sherman <zsherman@anl.gov>
2018-05-10 12:39:01 -0500

    MAINT: Reintroduce script to download data for the pyart_short_course.

--- provision_scripts/install_pyart_notebooks.sh
+++ provision_scripts/install_pyart_notebooks.sh
@@ -10,5 +10,5 @@ sudo apt-get install -qq zip unzip
 git clone https://github.com/openradar/AMS-Short-Course-on-Open-Source-Radar-Software.git pyart_short_course
 
 # Optionally get data, add ~1 GB to the VM size
-#cd pyart_short_course/data
-#./provision_data.sh
+cd pyart_short_course/data
+./provision_data.sh
