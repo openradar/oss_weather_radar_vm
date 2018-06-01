2888cf1018d3b6786823dbb87f4d78d75552c615
heistermann <maik.heistermann@uni-potsdam.de>
2018-06-01 09:42:06 +0000

    Install autoconf and enforce autoreconf

--- provision_scripts/install_baltrad_bbufr.sh
+++ provision_scripts/install_baltrad_bbufr.sh
@@ -7,6 +7,7 @@ set -x
 #sudo apt-get install -qq libproj0
 sudo apt-get install -qq proj-bin
 sudo apt-get install -qq libproj-dev
+sudo apt-get install -qq autoconf
 
 # install bbufr from source
 cd ~
@@ -16,6 +17,7 @@ fi
 cd tmp
 git clone --depth=1 git://git.baltrad.eu/bbufr.git
 cd bbufr/
+autoreconf -f -i
 ./configure --prefix=/opt/baltrad/bbufr LDFLAGS="-L/usr/lib/x86_64-linux-gnu/hdf5/serial" CFLAGS="-I/usr/include/hdf5/serial"
 make
 make install
