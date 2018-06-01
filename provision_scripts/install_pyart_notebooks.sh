5bbc6bd4fe6dcd4caf7833089fea29f045f19e4a
Zachary Sherman <zsherman@anl.gov>
2018-06-01 09:32:28 -0500

    MAINT: Changed repository where pyart notebooks reside. Old script would point to the AMS radar short course repository. A new pyart-notebooks repository was created with less content to fit the hour session time frame.

--- provision_scripts/install_pyart_notebooks.sh
+++ provision_scripts/install_pyart_notebooks.sh
@@ -7,8 +7,8 @@ cd ~
 # Install git and zip/unzip just in case.
 sudo apt-get install -qq git
 sudo apt-get install -qq zip unzip
-git clone https://github.com/openradar/AMS-Short-Course-on-Open-Source-Radar-Software.git pyart_short_course
+git clone https://github.com/EVS-ATMOS/pyart-notebooks.git pyart_short_course
 
-# Retrieve the data for the Py-ART course. Adds ~1 GB to the VM size.
+# Retrieve the data for the Py-ART course. Adds ~0.7 GB to the VM size.
 cd pyart_short_course/data
 ./provision_data.sh
