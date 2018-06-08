ddce2716152cae1a439e1d066f164f9f3a77fa2a
Scott Collis <scollis.acrf@gmail.com>
2018-06-08 10:24:56 -0500

    FIX: Removed dup installs

--- provision_scripts/install_intro_notebooks.sh
+++ provision_scripts/install_intro_notebooks.sh
@@ -4,6 +4,4 @@ set -x
 # Get the intro notebooks from GitHub.
 cd ~
 # Install git and zip/unzip just in case.
-sudo apt-get install -qq git
-sudo apt-get install -qq zip unzip
 git clone https://github.com/EVS-ATMOS/introductory_python
